49acb2e7cf0b48ce0bacb6053ccf199875e8749e
Peter Eichman <peichman@umd.edu>
2026-04-21 09:32:22 -0400

    LIBIIIF-225. Include query string (if present) when redirecting to `/manifest`.
    Added additional tests of the web application requests.
    
    https://umd-dit.atlassian.net/browse/LIBIIIF-225

---
+++ tests/test_web.py
@@ -0,0 +1,65 @@
+from http import HTTPStatus
+
+import pytest
+
+from papaya import __version__
+from papaya.web import create_app
+
+
+@pytest.fixture
+def app(monkeypatch):
+    monkeypatch.setenv('PAPAYA_SOLR_ENDPOINT', 'http://localhost:8985/solr/fcrepo')
+    monkeypatch.setenv('PAPAYA_SOLR_TEXT_MATCH_FIELD', 'extracted_text__dps_txt')
+    monkeypatch.setenv('PAPAYA_FCREPO_ENDPOINT', 'http://fcrepo-local:8080/fcrepo/rest')
+    monkeypatch.setenv('PAPAYA_FCREPO_PREFIX', 'fcrepo:')
+    monkeypatch.setenv('PAPAYA_IIIF_IMAGE_ENDPOINT', 'http://localhost:8182/iiif/2')
+    monkeypatch.setenv('PAPAYA_URL', 'http://localhost/manifests')
+    return create_app()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_root(client):
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.headers['Location'] == 'http://localhost/manifests/'
+
+
+def test_manifests_form(client):
+    response = client.get('/manifests/')
+    assert response.status_code == HTTPStatus.OK
+    assert 'Papaya' in response.text
+    assert __version__ in response.text
+
+
+def test_find_manifest(client):
+    response = client.post('/manifests/', data={'uri': 'http://fcrepo-local:8080/fcrepo/rest/123'})
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.headers['Location'] == 'http://localhost/manifests/fcrepo:123/manifest'
+
+
+def test_find_manifest_with_query(client):
+    response = client.post('/manifests/', data={'uri': 'http://fcrepo-local:8080/fcrepo/rest/123', 'text_query': 'foo'})
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.headers['Location'] == 'http://localhost/manifests/fcrepo:123/manifest?q=foo'
+
+
+@pytest.mark.parametrize(
+    ('request_path', 'canonical_url'),
+    [
+        ('/manifests/foobar/manifest.json', 'http://localhost/manifests/foobar/manifest'),
+        ('/manifests/foobar/', 'http://localhost/manifests/foobar/manifest'),
+        # preserves query string
+        ('/manifests/foobar/manifest.json?q=swordfish', 'http://localhost/manifests/foobar/manifest?q=swordfish'),
+        ('/manifests/foobar/?q=swordfish', 'http://localhost/manifests/foobar/manifest?q=swordfish'),
+        ('/manifests/foobar/manifest.json?ANY_STRING', 'http://localhost/manifests/foobar/manifest?ANY_STRING'),
+        ('/manifests/foobar/?ANY_STRING', 'http://localhost/manifests/foobar/manifest?ANY_STRING'),
+    ]
+)
+def test_redirect_to_manifest(client, request_path, canonical_url):
+    response = client.get(request_path)
+    assert response.status_code == HTTPStatus.MOVED_PERMANENTLY
+    assert response.headers['Location'] == canonical_url
